dc85f2bfb5c727c41a84b6c1bd30db158f1fadb4
Kirill777-web <ki.lazutin@gmail.com>
2024-02-02 17:30:51 -0400

    code changes

--- src/hooks/useSubmit.js
+++ src/hooks/useSubmit.js
@@ -4,36 +4,29 @@ import { useAlertContext } from '../context/alertContext';
 
 const useSubmit = () => {
   const [isLoading, setLoading] = useState(false);
-  const { onOpen } = useAlertContext(); // Use the alert context
+  const { onOpen } = useAlertContext();
 
   const LAMBDA_URL = process.env.REACT_APP_LAMBDA_INVOKE_URL;
 
   const submit = async (formData, resetForm) => {
     setLoading(true);
     try {
-      // Prepare the payload with email data
       const payload = {
         to: process.env.REACT_APP_TARGET_EMAIL,
         subject: 'New Inquiry from ' + formData.firstName,
         body: `Name: ${formData.firstName}\nEmail: ${formData.email}\nType: ${formData.type}\nComment: ${formData.comment}`,
       };
 
-      const res = await axios.post(LAMBDA_URL, payload);
-      setResponse({
-        type: 'success',
-        message: 'Email sent successfully',
-      });
-      resetForm(); // Reset form fields after successful submission
+      await axios.post(LAMBDA_URL, payload);
+      onOpen('success', 'Email sent successfully');
+      resetForm();
     } catch (error) {
       let errorMessage = 'Error occurred while sending email.';
       if (error.response && error.response.data) {
         errorMessage =
           error.response.data.message || JSON.stringify(error.response.data);
       }
-      setResponse({
-        type: 'error',
-        message: errorMessage,
-      });
+      onOpen('error', errorMessage);
     } finally {
       setLoading(false);
     }
