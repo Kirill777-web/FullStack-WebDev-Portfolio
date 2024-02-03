8aa8ff5b00d4baf8e624be32d425acfa7c7eea95
Kirill777-web <ki.lazutin@gmail.com>
2024-02-02 21:46:54 -0400

    useSubmit.js updates

--- src/hooks/useSubmit.js
+++ src/hooks/useSubmit.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import axios from 'axios';
+
 import { useAlertContext } from '../context/alertContext';
 
 const useSubmit = () => {
   const [isLoading, setLoading] = useState(false);
+  const [response, setResponse] = useState(null);
   const { onOpen } = useAlertContext();
 
   const LAMBDA_URL = process.env.REACT_APP_LAMBDA_INVOKE_URL;
@@ -16,11 +18,14 @@ const useSubmit = () => {
         subject: 'New Inquiry from ' + formData.firstName,
         body: `Name: ${formData.firstName}\nEmail: ${formData.email}\nType: ${formData.type}\nComment: ${formData.comment}`,
       };
+      console.log('Sending request to Lambda:', LAMBDA_URL, payload);
+      const res = await axios.post(LAMBDA_URL, payload);
+      console.log('Response from Lambda:', res.data);
 
-      await axios.post(LAMBDA_URL, payload);
       onOpen('success', 'Email sent successfully');
       resetForm();
     } catch (error) {
+      console.error('Error in useSubmit:', error);
       let errorMessage = 'Error occurred while sending email.';
       if (error.response && error.response.data) {
         errorMessage =
