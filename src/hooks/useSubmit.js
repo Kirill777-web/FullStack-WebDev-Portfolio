f6c4b5cb0c5ce89abbe0147c864f8b7f4c0c3bd2
Kirill777-web <ki.lazutin@gmail.com>
2024-02-02 22:12:48 -0400

    modification in useSubmit.js

--- src/hooks/useSubmit.js
+++ src/hooks/useSubmit.js
@@ -1,39 +1,43 @@
 import { useState } from 'react';
 import axios from 'axios';
-//import dotenv
-require('dotenv').config();
+
+import { useAlertContext } from '../context/alertContext';
 
 const useSubmit = () => {
   const [isLoading, setLoading] = useState(false);
   const [response, setResponse] = useState(null);
+  const { onOpen } = useAlertContext();
 
-  // Replace 'YOUR_LAMBDA_INVOKE_URL' with your actual Lambda function's URL
   const LAMBDA_URL = process.env.REACT_APP_LAMBDA_INVOKE_URL;
 
-  const submit = async (data) => {
+  const submit = async (formData, resetForm) => {
     setLoading(true);
     try {
-      const res = await axios.post(LAMBDA_URL, {
-        to: process.env.REACT_APP_EMAIL, // Target email
-        subject: 'New Inquiry', // Subject from the form data (if you want to use)
-        body: JSON.stringify(data), // Convert form data to string
-      });
+      const payload = {
+        to: process.env.REACT_APP_TARGET_EMAIL,
+        subject: 'New Inquiry from ' + formData.firstName,
+        body: `Name: ${formData.firstName}\nEmail: ${formData.email}\nType: ${formData.type}\nComment: ${formData.comment}`,
+      };
+      console.log('Sending request to Lambda:', LAMBDA_URL, payload);
+      const res = await axios.post(LAMBDA_URL, payload);
+      console.log('Response from Lambda:', res.data);
+
+      onOpen('success', 'Email sent successfully');
+      resetForm();
     } catch (error) {
-      let errorMessage = 'Something went wrong, please try again later!';
-      if (error.response) {
-        errorMessage = error.response.data
-          ? error.response.data.message || JSON.stringify(error.response.data)
-          : errorMessage;
+      console.error('Error in useSubmit:', error);
+      let errorMessage = 'Error occurred while sending email.';
+      if (error.response && error.response.data) {
+        errorMessage =
+          error.response.data.message || JSON.stringify(error.response.data);
       }
-      setResponse({
-        type: 'error',
-        message: errorMessage,
-      });
+      onOpen('error', errorMessage);
     } finally {
       setLoading(false);
     }
   };
 
-  return { isLoading, response, submit };
+  return { isLoading, submit };
 };
+
 export default useSubmit;
